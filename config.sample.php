8e6459acf2923f008f9061dc924f4bf684cfb98a
devzrepo-main <devzrepo@gmail.com>
2025-11-11 11:05:11 -0600

    Create config.sample.php

---
+++ config.sample.php
@@ -0,0 +1,9 @@
+<?php
+// Copy this file to config.php and set your DB credentials
+return [
+  'host' => 'localhost',
+  'db'   => 'crashout_db',
+  'user' => 'root',      // change if you use a non-root user
+  'pass' => '',          // set your MariaDB password
+  'charset' => 'utf8mb4'
+];
